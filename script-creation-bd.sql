3f3c6e48d1604301332f9a56ec4783c3e2741f2e
rigobertoLemus <118749703+rigobertoLemus@users.noreply.github.com>
2023-10-04 12:36:59 -0400

    1push

---
+++ script-creation-bd.sql
@@ -0,0 +1,16 @@
+--Dans CDB
+--Creation de la base de données principale
+CREATE PLUGGABLE DATABASE PDB_ECOLE ADMIN USER PDB_ADMINISTRATEUR IDENTIFIED BY ORACLE
+FILE_NAME_CONVERT=('/opt/oracle/oradata/FREE/pdbseed/', '/opt/oracle/oradata/FREE/pdb_ecole/');
+
+--Verification de la creation de la base de donées
+SELECT
+    PDB_NAME,
+    STATUS
+FROM
+    DBA_PDBS
+ORDER BY
+    PDB_NAME;
+
+--Creation de la copie de la base de donées
+CREATE USER C##COPIE_PDB_ECOLE IDENTIFIED BY oracle;
